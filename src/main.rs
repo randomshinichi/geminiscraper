c448303cf890953f01068394a07a6ebaf6ee2919
Andrew Chiw <randomshinichi4869@gmail.com>
2022-10-08 08:19:42 +0200

    separate getting links from a page vs downloading them

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate unqlite;
 
-use unqlite::{UnQLite,KV, Config, Cursor};
-use gmi::url::{Path, Url};
-use gmi::request;
 use gmi::gemtext::{parse_gemtext, GemtextNode};
+use gmi::request;
+use gmi::url::{Path, Url};
 use log::*;
-use std::sync::{Arc,Mutex};
+use std::sync::{Arc, Mutex};
+use unqlite::{Config, Cursor, UnQLite, KV};
 
 type Db = Arc<Mutex<UnQLite>>;
 
@@ -24,74 +24,90 @@ async fn main() {
     match command.as_str() {
         "scan" => {
             let url = Url::try_from(u.as_str()).expect("url couldn't be parsed");
-            let page = download(url.clone()).await.expect("couldn't download gemini url");
+            let page = download(url.clone())
+                .await
+                .expect("couldn't download gemini url");
 
-            download_links_in_page(url, page.to_string(), db).await;
-        },
+            let links = get_links(url, page.to_string()).await;
+            println!("{:?}", links);
+
+            download_links(links, db).await;
+        }
         "read" => {
             let root = args[3].clone();
             let url = Url::try_from(root.as_str()).expect("url couldn't be parsed");
             // use fs::read_to_string to open gemlog.gmi
-            let contents = std::fs::read_to_string(u.clone()).expect("Something went wrong reading the file");
-
-            download_links_in_page(url, contents.to_string(), db).await;
-        },
+            let contents =
+                std::fs::read_to_string(u.clone()).expect("Something went wrong reading the file");
+            let links = get_links(url, contents).await;
+            download_links(links, db).await;
+        }
         _ => {
             println!("Command not found");
         }
     }
 }
 
-async fn download_links_in_page(url: Url, page: String, db: Db) {
+async fn get_links(url: Url, page: String) -> Vec<String> {
+    let mut links = vec![];
     let gmi = parse_gemtext(&page.to_string());
     for node in gmi {
-        let db = db.clone();
         let mut url = url.clone();
         match node {
             GemtextNode::Link(link, _) => {
                 // only download .gmi links
-                if !link.contains(".gmi") {
-                    continue;
-                }
+                // if !link.contains(".gmi") {
+                //     continue;
+                // }
 
-                // link is just "filename.gmi". Merge it with
+                // link is just "dir/filename.gmi". Merge it with
                 // gemini://hostname/phlog/ to get the full path
                 match url.path {
-                    Some(path)  => {
+                    Some(path) => {
                         url.path = Some(path.merge_path(&Path::try_from(link.as_str()).unwrap()));
-                    },
+                    }
                     None => {
                         url.path = Some(Path::try_from(link.as_str()).unwrap());
                     }
                 }
                 info!("found path {}", url.clone().to_string());
-
-                // check if db already contains the page
-                let db_readonly = db.lock().unwrap();
-                if db_readonly.kv_contains(url.to_string()) {
-                    warn!("db already contains {}", url.to_string());
-                    continue
-                }
-                drop(db_readonly);
-
-                tokio::spawn(async move {
-                    let page = download(url.clone()).await;
-                    match page {
-                        // match page
-                        Ok(s) => {
-                            // lock the Arc and Mutex
-                            let db = db.lock().unwrap();
-                            db.kv_store(url.to_string(), s).unwrap();
-                        },
-                        Err(_) => ()
-                    }
-                });
-            },
-            _ => ()
+                links.push(url.to_string());
+            }
+            _ => (),
         }
-    };
+    }
+    return links;
 }
 
+async fn download_links(links: Vec<String>, db: Db) {
+    for link in links {
+        let db = Arc::clone(&db);
+
+        // check if db already contains the page
+        let db_readonly = db.lock().unwrap();
+        if db_readonly.kv_contains(link.clone()) {
+            warn!("db already contains {}", link);
+            continue;
+        }
+        drop(db_readonly);
+
+        tokio::spawn(async move {
+            let page = download(
+                Url::try_from(link.as_str()).expect("couldn't convert a String into a URL"),
+            )
+            .await;
+            match page {
+                // match page
+                Ok(s) => {
+                    // lock the Arc and Mutex
+                    let db = db.lock().unwrap();
+                    db.kv_store(link, s).unwrap();
+                }
+                Err(_) => (),
+            }
+        });
+    }
+}
 
 async fn download(url: Url) -> Result<String, Box<dyn std::error::Error>> {
     // use gmi to get a page
@@ -103,10 +119,10 @@ async fn download(url: Url) -> Result<String, Box<dyn std::error::Error>> {
             // match s, if it is a String or a str
             let s = match s {
                 std::borrow::Cow::Borrowed(s) => s.to_string(),
-                std::borrow::Cow::Owned(s) => s
+                std::borrow::Cow::Owned(s) => s,
             };
             Ok(s)
-        },
+        }
         Err(err) => {
             error!("couldn't download {}", url);
             Err(Box::new(err))
